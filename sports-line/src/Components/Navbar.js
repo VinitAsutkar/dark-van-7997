a355e75a67dca9c374d9a83fb0c4978c007d13a3
VinitAsutkar <vinit123asutkar@gmail.com>
2023-01-21 21:56:24 +0530

    Logout

--- sports-line/src/Components/Navbar.js
+++ sports-line/src/Components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar=()=>{
         <div id="Logo"><Link to={"/"}><img style={{width:"100%"}} src="https://cdn.worldvectorlogo.com/logos/sports-line.svg" alt="Logo"/></Link></div>
         <Link to={"/products"} style={{textDecoration:"none",color:"black"}}><span id="Products">Products</span></Link>
         <Link to={"/cart"} style={{textDecoration:"none",color:"black"}}><div id="Cart"><i class="fa-sharp fa-solid fa-cart-shopping"></i><span id="Cart-Count">0</span><span> Cart</span></div></Link>
-        <Link to={"/login"} style={{textDecoration:"none",color:"black"}}><div id="User"><i class="fa-solid fa-user"></i><span id="Profile">{JSON.parse(localStorage.getItem("Profile"))}</span></div></Link>
+        <Link to={"/login"} style={{textDecoration:"none",color:"black"}}><div id="User"><i class="fa-solid fa-user"></i><span id="Profile">{JSON.parse(localStorage.getItem("Profile"))||"Login"}</span></div></Link>
         <div style={{display:IsAuth?true:"none"}} onClick={()=>Logout()}><span id="Logout">Logout <i class="fa-sharp fa-solid fa-power-off"></i></span></div>
     </div>
 }
--- sports-line/src/Context/AuthContextProvider.js
+++ sports-line/src/Context/AuthContextProvider.js
@@ -1,12 +1,13 @@
 import React from "react";
 export const AuthContext=React.createContext();
 const AuthContextProvider=({children})=>{
-    const [IsAuth,SetIsAuth]=React.useState(false);
+    const [IsAuth,SetIsAuth]=React.useState(JSON.parse(localStorage.getItem("Profile"))||false);
     const Login=()=>{
         SetIsAuth(true)
     }
     const Logout=()=>{
         SetIsAuth(false)
+        localStorage.setItem("Profile",null)
     }
     return <AuthContext.Provider value={{IsAuth,Login,Logout}}>{children}</AuthContext.Provider>
 }
